1c610039edd1999d103490abf9c0573ed8d0294d
Exypte <38421281+Exypte@users.noreply.github.com>
2018-05-28 11:27:30 +0200

    M.proper

--- app/src/main/java/fr/univ_angers/agenda_ua/Utilisateur.java
+++ app/src/main/java/fr/univ_angers/agenda_ua/Utilisateur.java
@@ -4,7 +4,6 @@ package fr.univ_angers.agenda_ua;
  * Created by Thibault Condemine on 23/05/2018.
  */
 public class Utilisateur {
-    private long _id;
     private String _lien;
     private String _formation;
 
@@ -14,10 +13,6 @@ public class Utilisateur {
     }
 
     //GETTER
-    public long get_id() {
-        return _id;
-    }
-
     public String get_lien() {
         return _lien;
     }
@@ -27,10 +22,6 @@ public class Utilisateur {
     }
 
     //SETTER
-    public void set_id(long id) {
-        this._id = id;
-    }
-
     public void set_lien(String lien) {
         this._lien = lien;
     }
